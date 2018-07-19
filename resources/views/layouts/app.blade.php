e491e51027bc9fdd743a1b2615d171d5a11ffbb9
Hillary Lavin <hilavin@gmail.com>
2018-07-19 14:17:21 -0400

    Set-up navbar and jumbotron

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,13 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/app.css')}}"
-        <title>{{config('app.name', 'My First Laravel Blog')}}</title>
+        {{-- <title>{{config('app.name', 'My First Laravel Blog')}}</title> --}}
 
     </head>
     <body>
-        @yield('content')
+        @include('inc.navbar');
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,8 +2,11 @@
 
 
 @section('content')
+    <div class="jumbotron text-center">
         <h1>{{$title}}</h1>
         <p>This is the laravel application from Traversy Media's YouTube series, "Laravel from Scratch"</p>
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+    </div>
 @endsection
 
 
