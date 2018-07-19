b7fef1ace3445551bfb7fae8cafb3031e791e8ea
Hillary Lavin <hilavin@gmail.com>
2018-07-19 13:10:35 -0400

    Add layout (app), convert about, index, services into extensions)

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,15 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'My First Laravel Blog')}}</title>
-
-    </head>
-    <body>
+@section('content')
         <h1>Services</h1>
         <p>This is the Services page</p>
-    </body>
-</html>
+@endsection
