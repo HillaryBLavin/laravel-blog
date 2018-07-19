d426c127522352eb3eb628c724bf56a184934046
Hillary Lavin <hilavin@gmail.com>
2018-07-19 19:06:08 -0400

    Change default column names in Post Model (example)

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // Table Name
+    protected $table = 'posts'; // This is how you would change the default table name
+    // Primary Key
+    public $primaryKey = 'id';
+    // Timestamps
+    public $timestamps = true;
 }
