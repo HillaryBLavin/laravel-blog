da817ea53d9b4d45cc7f1e75dec2849951bef450
Hillary Lavin <hilavin@gmail.com>
2018-07-19 14:56:57 -0400

    Add resource for all the CRUD routes!

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');  
 Route::get('/services', 'PagesController@services');  
 
+Route::resource('posts', 'PostsController');
+
 
 
 // EXAMPLE - Dynamic route to retrieve specific user
