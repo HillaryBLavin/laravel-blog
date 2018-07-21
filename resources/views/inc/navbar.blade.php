33363245e0d11c1dcb37ab948900c55ec29ac539
Hillary Lavin <hilavin@gmail.com>
2018-07-20 21:35:48 -0400

    Add dashboard link in dropdown when logged in

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -40,7 +40,8 @@
                       </a>
 
                       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                          <a class="dropdown-item" href="{{ route('logout') }}"
+                        <a class="dropdown-item" href="/dashboard">Dashboard</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
                               onclick="event.preventDefault();
                                             document.getElementById('logout-form').submit();">
                               {{ __('Logout') }}
@@ -49,6 +50,7 @@
                           <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                               @csrf
                           </form>
+
                       </div>
                   </li>
               @endguest
